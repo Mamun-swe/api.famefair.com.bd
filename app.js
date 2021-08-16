dc72cda15cde8e70c5c8116034b9b05d38da6cab
Mamun-swe <mhmamun166009@gmail.com>
2021-08-16 23:40:07 +0600

    changes in yml file

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ const Route = require('./api/Routes/Index')
 app.use("/api/v1/", Route)
 
 app.get('/', async (req, res) => {
-    res.send("Hello I am node.js application!! WOW")
+    res.send("Hello I am node.js application!! WOW ")
 })
 
 app.use((req, res, next) => {
